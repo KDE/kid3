9a6094119224111ee534a425b9f1207affb2f2f8
Urs Fleisch <ufleisch@users.sourceforge.net>
2004-07-31 08:23:39 +0000

    MiscConfig() options for rename directory format
    writeToConfig()
    write window width and height, splitter size for Qt only version
    
    readFromConfig()
    read window width and height, splitter size for Qt only version

--- kid3/kid3/miscconfig.cpp
+++ kid3/kid3/miscconfig.cpp
@@ -20,6 +20,8 @@
 #endif
 
 #include "filelist.h"
+#include "mp3file.h"
+#include "rendirdialog.h"
 #include "miscconfig.h"
 
 /**
@@ -33,6 +35,8 @@ MiscConfig::MiscConfig(const QString &group) : GeneralConfig(group)
 	nameFilter = FileList::defaultNameFilter;
 	formatItem = 0;
 	formatText = Mp3File::fnFmtList[0];
+	dirFormatItem = 0;
+	dirFormatText = RenDirDialog::dirFmtList[0];
 }
 
 /**
@@ -59,6 +63,8 @@ void MiscConfig::writeToConfig(
 	config->writeEntry("NameFilter", nameFilter);
 	config->writeEntry("FormatItem", formatItem);
 	config->writeEntry("FormatText", formatText);
+	config->writeEntry("DirFormatItem", dirFormatItem);
+	config->writeEntry("DirFormatText", dirFormatText);
 	config->writeEntry("FormatWhileEditing", formatWhileEditing);
 	config->writeEntry("SplitterSizes", splitterSizes);
 #else
@@ -66,7 +72,22 @@ void MiscConfig::writeToConfig(
 	config->writeEntry("/NameFilter", nameFilter);
 	config->writeEntry("/FormatItem", formatItem);
 	config->writeEntry("/FormatText", formatText);
+	config->writeEntry("/DirFormatItem", dirFormatItem);
+	config->writeEntry("/DirFormatText", dirFormatText);
 	config->writeEntry("/FormatWhileEditing", formatWhileEditing);
+#if QT_VERSION >= 300
+	QValueList<int>::const_iterator it;
+#else
+	QValueListConstIterator<int> it;
+#endif
+	int i;
+	for (it = splitterSizes.begin(), i = 0;
+		 it != splitterSizes.end();
+		 ++it, ++i) {
+		config->writeEntry("/SplitterSize" + QString::number(i), *it);
+	}
+	config->writeEntry("/WindowWidth", windowWidth);
+	config->writeEntry("/WindowHeight", windowHeight);
 	config->endGroup();
 #endif
 }
@@ -91,10 +112,14 @@ void MiscConfig::readFromConfig(
 	    config->readEntry("NameFilter", FileList::defaultNameFilter);
 	formatItem =
 	    config->readNumEntry("FormatItem", 0);
+	dirFormatItem =
+	    config->readNumEntry("DirFormatItem", 0);
 	formatWhileEditing = config->readBoolEntry("FormatWhileEditing", formatWhileEditing);
 #if QT_VERSION >= 300
 	formatText =
 	    config->readEntry("FormatText", Mp3File::fnFmtList[0]);
+	dirFormatText =
+	    config->readEntry("DirFormatText", RenDirDialog::dirFmtList[0]);
 #endif
 	splitterSizes = config->readIntListEntry("SplitterSizes");
 #else
@@ -103,11 +128,33 @@ void MiscConfig::readFromConfig(
 	    config->readEntry("/NameFilter", FileList::defaultNameFilter);
 	formatItem =
 	    config->readNumEntry("/FormatItem", 0);
+	dirFormatItem =
+	    config->readNumEntry("/DirFormatItem", 0);
 	formatWhileEditing = config->readBoolEntry("/FormatWhileEditing", formatWhileEditing);
 #if QT_VERSION >= 300
 	formatText =
 	    config->readEntry("/FormatText", Mp3File::fnFmtList[0]);
+	dirFormatText =
+	    config->readEntry("/DirFormatText", RenDirDialog::dirFmtList[0]);
+	QValueList<int>::iterator it;
+#else
+	QValueListIterator<int> it;
 #endif
+	splitterSizes.clear();
+	for (int i = 0; i < 5; ++i) {
+		int val = config->readNumEntry("/SplitterSize" + QString::number(i), -1);
+		if (val != -1) {
+#if QT_VERSION >= 300
+			splitterSizes.push_back(val);
+#else
+			splitterSizes.append(val);
+#endif
+		} else {
+			break;
+		}
+	}
+	windowWidth = config->readNumEntry("/WindowWidth", -1);
+	windowHeight = config->readNumEntry("/WindowHeight", -1);
 	config->endGroup();
 #endif
 }
