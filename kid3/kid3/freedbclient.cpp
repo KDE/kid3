7277f0c6df9aa409c2ec3bc4f0a7280e4642759f
Urs Fleisch <ufleisch@users.sourceforge.net>
2006-10-19 04:06:15 +0000

    moved common import from external server behavior to base class ImportSourceClient

--- kid3/kid3/freedbclient.cpp
+++ kid3/kid3/freedbclient.cpp
@@ -1,55 +1,24 @@
 /**
  * \file freedbclient.cpp
  * freedb.org client.
- * Originalliy implemented using QHttp, but this does not work on Qt < 3.0
- * and it hanged on connection errors or going via a proxy. Thus it is
- * now done using QSocket, which is not much more complicated.
  *
  * \b Project: Kid3
  * \author Urs Fleisch
  * \date 18 Jan 2004
  */
 
-#include "config.h"
-#ifdef CONFIG_USE_KDE
-#include <klocale.h>
-#else
-#define i18n(s) tr(s)
-#define I18N_NOOP(s) QT_TR_NOOP(s)
-#endif
-
-#include <qregexp.h>
-#include <qsocket.h>
-#include <qstatusbar.h>
-#include <qurl.h>
-#include "freedbconfig.h"
 #include "freedbclient.h"
+#include "importsourceconfig.h"
+#include <qregexp.h>
+#include <qurl.h>
 
-#if defined WIN32 && QT_VERSION < 300
-// see hostnameToAddress()
-#include <winsock.h>
-#endif
+static const char freedb2Server[] = "freedb2.org:80";
 
 /**
  * Constructor.
- *
- * @param sb status bar to display progress information.
  */
-FreedbClient::FreedbClient(QStatusBar *sb) :
- statusBar(sb), m_requestType(RT_None)
+FreedbClient::FreedbClient()
 {
-	sock = new QSocket();
-	connect(sock, SIGNAL(hostFound()),
-			this, SLOT(slotHostFound()));
-	connect(sock, SIGNAL(connected()),
-			this, SLOT(slotConnected()));
-	connect(sock, SIGNAL(connectionClosed()),
-			this, SLOT(slotConnectionClosed()));
-	connect(sock, SIGNAL(readyRead()),
-			this, SLOT(slotReadyRead()));
-	connect(sock, SIGNAL(error(int)),
-			this, SLOT(slotError(int)));
-	statusBar->message(i18n("Ready."));
 }
 
 /**
@@ -57,248 +26,87 @@ FreedbClient::FreedbClient(QStatusBar *sb) :
  */
 FreedbClient::~FreedbClient()
 {
-	sock->close();
-	sock->disconnect();
-	delete sock;
 }
 
 /**
- * Get string with proxy or destination and port.
- * If a proxy is set, the proxy is returned, else the real destination.
+ * Construct a query command in m_request to search on the server.
  *
- * @param cfg  freedb configuration
- * @param dst real destination
- *
- * @return "destinationname:port".
+ * @param cfg      import source configuration
+ * @param artist   artist to search
+ * @param album    album to search
+ * @param dest     the server to connect to is returned here
+ * @param destPort the port of the server is returned here
  */
-static QString getProxyOrDest(const FreedbConfig *cfg, const QString dst)
+void FreedbClient::constructFindQuery(
+	const ImportSourceConfig* cfg,
+	const QString& artist, const QString& album,
+	QString& dest, int& destPort)
 {
-	QString dest;
-	if (cfg->useProxy) {
-		dest = cfg->proxy;
-	}
-	if (dest.isEmpty()) {
-		dest = dst;
-	}
-	return dest;
-}
-
-/**
- * Extract name and port from string.
- *
- * @param namePort input string with "name:port"
- * @param name     output string with "name"
- * @param port     output integer with port
- */
-void FreedbClient::splitNamePort(const QString &namePort,
-																 QString &name, int &port)
-{
-	int colPos = namePort.findRev(':');
-	if (colPos >= 0) {
-		bool ok;
-		port = namePort.mid(colPos + 1).toInt(&ok);
-		if (!ok) port = 80;
-		name = namePort.left(colPos);
-	} else {
-		name = namePort;
-		port = 80;
-	}
-}
-
-#if defined WIN32 && QT_VERSION < 300
-/**
- * Lookup hostname.
- * connectToHost() does not seem to look up the host names correctly on
- * Qt 2.3 non commerical Windows, so some Windows specific code is used here.
- *
- * @param adr input: hostname, output: IP address string
- *
- * @return 0 if ok,
- *         WINSOCK error code if error.
- */
-static int hostnameToAddress(QString &adr)
-{
-	WSADATA wsaData;
-	int err = WSAStartup(MAKEWORD(1, 1), &wsaData);
-	if (err == 0) {
-		struct hostent FAR *he = gethostbyname(adr.latin1());
-		if (he) {
-			adr = QHostAddress(htonl(*(u_long *)he->h_addr_list[0])).toString();
-		} else {
-			err = WSAGetLastError();
-		}
-		(void)WSACleanup();
-	}
-	return err;
-}
-#endif
-
-/**
- * Find keyword in freedb.
- *
- * @param cfg  freedb configuration
- * @param what string with words to search
- */
-void FreedbClient::find(const FreedbConfig *cfg, QString what)
-{
-	QString destNamePort(getProxyOrDest(cfg, "www.freedb.org:80"));
-	QString dest;
-	int destPort;
+	// At the moment, only freedb2.org recognizes cddb album commands,
+	// so we always use this server for find queries.
+	QString server(freedb2Server);
+	QString what = artist + " / " + album;
+	QString destNamePort(getProxyOrDest(server));
 	splitNamePort(destNamePort, dest, destPort);
-	what.replace(QRegExp(" +"), "+"); // replace spaces by '+'
-	request = "GET http://www.freedb.org/freedb_search.php?words=" + what +
-		"&allfields=NO&fields=artist&fields=title&allcats=NO" +
-		"&cats=blues&cats=classical&cats=country&cats=folk&cats=jazz" +
-		"&cats=misc&cats=newage&cats=reggae&cats=rock&cats=soundtrack" +
-		"&grouping=none HTTP/1.1\r\nHost: " +
-		cfg->server + "\r\nUser-Agent: Kid3 " VERSION "\r\nConnection: close\r\n\r\n";
-#if defined WIN32 && QT_VERSION < 300
-	int err = hostnameToAddress(dest);
-	if (err) {
-		statusBar->message(QString("WinSock error %1").arg(err));
-		return;
-	}
-#endif
-	sock->connectToHost(dest, destPort);
-	m_requestType = RT_FindFreedbSearch;
-
-	statusBar->message(i18n("Connecting..."));
-}
-
-/**
- * Find keyword in freedb with "cddb album" command of freedb2.org.
- *
- * @param cfg  freedb configuration
- * @param what string with words to search
- */
-void FreedbClient::findCddbAlbum(const FreedbConfig *cfg, QString what)
-{
-	QString destNamePort(getProxyOrDest(cfg, cfg->server));
-	QString dest;
-	int destPort;
-	splitNamePort(destNamePort, dest, destPort);
+	QString serverName;
+	int serverPort;
+	splitNamePort(server, serverName, serverPort);
 	what.replace(QRegExp(" +"), " "); // collapse spaces
 	QUrl::encode(what);
 	what.replace("%20", "+"); // replace spaces by '+'
-	request = "GET http://" + cfg->server + cfg->cgiPath +
-		"?cmd=cddb+album+" + what + "&hello=noname+localhost+" +
-		"Kid3+" VERSION "&proto=1 HTTP/1.1\r\nHost: " + cfg->server +
-		"\r\nConnection: close\r\n\r\n";
-#if defined WIN32 && QT_VERSION < 300
-	int err = hostnameToAddress(dest);
-	if (err) {
-		statusBar->message(QString("WinSock error %1").arg(err));
-		return;
+	m_request = "GET ";
+	if (dest != serverName) {
+		m_request += "http://";
+		m_request += serverName;
+		if (serverPort != 80) {
+			m_request += ':';
+			m_request += QString::number(serverPort);
+		}
 	}
-#endif
-	sock->connectToHost(dest, destPort);
-	m_requestType = RT_FindCddbAlbum;
-
-	statusBar->message(i18n("Connecting..."));
+	m_request += cfg->m_cgiPath;
+	m_request += "?cmd=cddb+album+";
+	m_request += what;
+	m_request += "&hello=noname+localhost+";
+	m_request += "Kid3+" VERSION "&proto=1 HTTP/1.1\r\nHost: ";
+	m_request += serverName;
+	m_request += "\r\nConnection: close\r\n\r\n";
 }
 
 /**
- * Display status if host is found.
- */
-void FreedbClient::slotHostFound()
-{
-	statusBar->message(i18n("Host found..."));
-}
-
-/**
- * Display status if connection is established.
- */
-void FreedbClient::slotConnected()
-{
-	sock->writeBlock(request.latin1(), request.length());
-	statusBar->message(i18n("Request sent..."));
-}
-
-/**
- * Read received data when the server has closed the connection.
- * The data is sent to other objects via signals.
- */
-void FreedbClient::slotConnectionClosed()
-{
-	Q_ULONG len = sock->bytesAvailable();
-	QCString s;
-	s.resize(len + 1);
-	sock->readBlock(s.data(), len);
-	rcvStr = QString::fromUtf8(s.data());
-	switch (m_requestType) {
-		case RT_Album:
-			emit albumFinished(rcvStr);
-			break;
-		case RT_FindFreedbSearch:
-			emit findFinished(rcvStr);
-			break;
-		case RT_FindCddbAlbum:
-			emit findCddbAlbumFinished(rcvStr);
-			break;
-		default:
-			qWarning("Unknown freedb request type");
-	}
-	sock->close();
-	statusBar->message(i18n("Ready."));
-}
-
-/**
- * Display information about read progress.
- */
-void FreedbClient::slotReadyRead()
-{
-	statusBar->message(i18n("Data received: %1").arg(sock->bytesAvailable()));
-}
-
-/**
- * Display information about socket error.
- */
-void FreedbClient::slotError(int err)
-{
-	QString msg(i18n("Socket error: "));
-	switch (err) {
-		case QSocket::ErrConnectionRefused:
-			msg += i18n("Connection refused");
-			break;
-		case QSocket::ErrHostNotFound:
-			msg += i18n("Host not found");
-			break;
-		case QSocket::ErrSocketRead:
-			msg += i18n("Read failed");
-			break;
-		default:
-			msg += QString::number(err);
-	}
-	statusBar->message(msg);
-}
-
-/**
- * Request track list from freedb server.
+ * Construct a query command in m_request to fetch the track list
+ * from the server.
  *
- * @param cfg freedb configuration
- * @param cat category
- * @param id  ID
+ * @param cfg      import source configuration
+ * @param cat      category
+ * @param id       ID
+ * @param dest     the server to connect to is returned here
+ * @param destPort the port of the server is returned here
  */
-void FreedbClient::getTrackList(const FreedbConfig *cfg, QString cat, QString id)
+void FreedbClient::constructTrackListQuery(
+	const ImportSourceConfig* cfg, const QString& cat, const QString& id,
+	QString& dest, int& destPort)
 {
-	QString destNamePort(getProxyOrDest(cfg, cfg->server));
-	QString dest;
-	int destPort;
+	QString destNamePort(getProxyOrDest(cfg->m_server));
 	splitNamePort(destNamePort, dest, destPort);
-	request = "GET http://" + cfg->server + cfg->cgiPath +
-		"?cmd=cddb+read+" + cat + "+" +
-		id + "&hello=noname+localhost+" +
-		"Kid3+" VERSION "&proto=1 HTTP/1.1\r\nHost: " + cfg->server +
-		"\r\nConnection: close\r\n\r\n";
-
-#if defined WIN32 && QT_VERSION < 300
-	int err = hostnameToAddress(dest);
-	if (err) {
-		statusBar->message(QString("WinSock error %1").arg(err));
-		return;
+	QString serverName;
+	int serverPort;
+	splitNamePort(cfg->m_server, serverName, serverPort);
+	m_request = "GET ";
+	if (dest != serverName) {
+		m_request += "http://";
+		m_request += serverName;
+		if (serverPort != 80) {
+			m_request += ':';
+			m_request += QString::number(serverPort);
+		}
 	}
-#endif
-	sock->connectToHost(dest, destPort);
-	m_requestType = RT_Album;
-	statusBar->message(i18n("Connecting..."));
+	m_request += cfg->m_cgiPath;
+	m_request += "?cmd=cddb+read+";
+	m_request += cat;
+	m_request += "+";
+	m_request += id;
+	m_request += "&hello=noname+localhost+";
+	m_request += "Kid3+" VERSION "&proto=1 HTTP/1.1\r\nHost: ";
+	m_request += serverName;
+	m_request += "\r\nConnection: close\r\n\r\n";
 }
--- kid3/kid3/freedbclient.h
+++ kid3/kid3/freedbclient.h
@@ -10,120 +10,51 @@
 #ifndef FREEDBCLIENT_H
 #define FREEDBCLIENT_H
 
-#include "config.h"
-#include <qobject.h>
-#include <qstring.h>
-
-class QStatusBar;
-class QSocket;
-class FreedbConfig;
+#include "importsourceclient.h"
 
 /**
  * freedb.org client.
  */
-class FreedbClient : public QObject
+class FreedbClient : public ImportSourceClient
 {
-Q_OBJECT
-
 public:
 	/**
 	 * Constructor.
-	 *
-	 * @param sb status bar to display progress information.
 	 */
-	FreedbClient(QStatusBar *sb);
+	FreedbClient();
+
 	/**
 	 * Destructor.
 	 */
 	virtual ~FreedbClient();
-	/**
-	 * Find keyword in freedb.
-	 *
-	 * @param cfg  freedb configuration
-	 * @param what string with words to search
-	 */
-	void find(const FreedbConfig *cfg, QString what);
-	/**
-	 * Find keyword in freedb with "cddb album" command of freedb2.org.
-	 *
-	 * @param cfg  freedb configuration
-	 * @param what string with words to search
-	 */
-	void findCddbAlbum(const FreedbConfig *cfg, QString what);
-	/**
-	 * Request track list from freedb server.
-	 *
-	 * @param cfg freedb configuration
-	 * @param cat category
-	 * @param id  ID
-	 */
-	void getTrackList(const FreedbConfig *cfg, QString cat, QString id);
 
 	/**
-	 * Extract name and port from string.
+	 * Construct a query command in m_request to search on the server.
 	 *
-	 * @param namePort input string with "name:port"
-	 * @param name     output string with "name"
-	 * @param port     output integer with port
+	 * @param cfg      import source configuration
+	 * @param artist   artist to search
+	 * @param album    album to search
+	 * @param dest     the server to connect to is returned here
+	 * @param destPort the port of the server is returned here
 	 */
-	static void splitNamePort(const QString &namePort,
-														QString &name, int &port);
+	virtual void constructFindQuery(
+		const ImportSourceConfig* cfg,
+		const QString& artist, const QString& album,
+		QString& dest, int& destPort);
 
-private slots:
 	/**
-	 * Display status if host is found.
+	 * Construct a query command in m_request to fetch the track list
+	 * from the server.
+	 *
+	 * @param cfg      import source configuration
+	 * @param cat      category
+	 * @param id       ID
+	 * @param dest     the server to connect to is returned here
+	 * @param destPort the port of the server is returned here
 	 */
-	void slotHostFound();
-	/**
-	 * Display status if connection is established.
-	 */
-	void slotConnected();
-	/**
-	 * Read received data when the server has closed the connection.
-	 * The data is sent to other objects via signals.
-	 */
-	void slotConnectionClosed();
-	/**
-	 * Display information about read progress.
-	 */
-	void slotReadyRead();
-	/**
-	 * Display information about socket error.
-	 */
-	void slotError(int err);
-
-signals:
-	/**
-	 * Emitted when find request finished.
-	 * Parameter: text containing result of find request
-	 */
-	void findFinished(QString);
-	/**
-	 * Emitted when findCddbAlbum request finished.
-	 * Parameter: text containing result of findCddbAlbum request
-	 */
-	void findCddbAlbumFinished(QString);
-	/**
-	 * Emitted when album track data request finished.
-	 * Parameter: text containing result of album request
-	 */
-	void albumFinished(QString);
-private:
-	/** status bar to display progress */
-	QStatusBar *statusBar;
-	/** client socket */
-	QSocket *sock;
-	/** request to set */
-	QString request;
-	/** buffer for received data */
-	QString rcvStr;
-	/** type of current request */
-	enum RequestType {
-		RT_None,
-		RT_FindFreedbSearch,
-		RT_FindCddbAlbum,
-		RT_Album
-	} m_requestType;
+	virtual void constructTrackListQuery(
+		const ImportSourceConfig* cfg, const QString& cat, const QString& id,
+		QString& dest, int& destPort);
 };
 
 #endif
