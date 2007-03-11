bc9f39b261a6410a9921860699a42c25654f69eb
Urs Fleisch <ufleisch@users.sourceforge.net>
2007-03-11 09:02:16 +0000

    new methods formatString(), replaceEscapedChars(), replacePercentCodes(), merge()

--- kid3/kid3/standardtags.cpp
+++ kid3/kid3/standardtags.cpp
@@ -8,6 +8,7 @@
  */
 
 #include "standardtags.h"
+#include "genres.h"
 
 StandardTags::StandardTags()
 {
@@ -90,6 +91,150 @@ void StandardTags::setEmpty()
 	genre = 0xff;
 	genreStr = "";
 }
+
+/**
+ * Copy tags which are empty or inactive from other tags.
+ * This can be used to merge two tags.
+ *
+ * @param st other tags
+ */
+void StandardTags::merge(const StandardTags& st)
+{
+	if (title.isEmpty())   title = st.title;
+	if (artist.isEmpty())  artist = st.artist;
+	if (album.isEmpty())   album = st.album;
+	if (comment.isEmpty()) comment = st.comment;
+	if (year <= 0)         year = st.year;
+	if (track <= 0)        track = st.track;
+	if ((genre == 0xff || genre == -1) && genreStr.isEmpty()) {
+		genre = st.genre;
+		genreStr = st.genreStr;
+	}
+}
+
+/**
+ * Replace escaped characters in a string.
+ *
+ * @param format string with escaped two-character-sequences
+ *               ("\n", "\t", "\r", "\\", "\a", "\b", "\f", "\v")
+ *
+ * @return string with escaped sequences replaced by characters.
+ */
+QString StandardTags::replaceEscapedChars(const QString& format)
+{
+	QString fmt(format);
+	if (!fmt.isEmpty()) {
+		const int numEscCodes = 8;
+		const QChar escCode[numEscCodes] = {
+			'n', 't', 'r', '\\', 'a', 'b', 'f', 'v'};
+		const char escChar[numEscCodes] = {
+			'\n', '\t', '\r', '\\', '\a', '\b', '\f', '\v'};
+
+		for (int pos = 0; pos < static_cast<int>(fmt.length());) {
+			pos = fmt.find('\\', pos);
+			if (pos == -1) break;
+			++pos;
+			for (int k = 0;; ++k) {
+				if (k >= numEscCodes) {
+					// invalid code at pos
+					++pos;
+					break;
+				}
+				if (fmt[pos] == escCode[k]) {
+					// code found, replace it
+					fmt.replace(pos - 1, 2, escChar[k]);
+					break;
+				}
+			}
+		}
+	}
+	return fmt;
+}
+
+/**
+ * Replace percent codes in a string.
+ *
+ * @param format string with percent codes
+ *               (% followed by a single character)
+ * @param codes        characters following percent
+ * @param replacements strings with replacements for codes
+ * @param numCodes     number of elements in codes and replacements
+ *
+ * @return string with percent codes replaced
+ */
+QString StandardTags::replacePercentCodes(
+	const QString& format, const QChar* codes,
+	const QString* replacements, int numCodes)
+{
+	QString fmt(format);
+	if (!fmt.isEmpty()) {
+		for (int pos = 0; pos < static_cast<int>(fmt.length());) {
+			pos = fmt.find('%', pos);
+			if (pos == -1) break;
+
+			for (int k = 0;; ++k) {
+				if (k >= numCodes) {
+					// invalid code at pos
+					++pos;
+					break;
+				}
+				if (fmt[pos + 1] == codes[k]) {
+					// code found, replace it
+					fmt.replace(pos, 2, replacements[k]);
+					pos += replacements[k].length();
+					break;
+				}
+			}
+		}
+	}
+	return fmt;
+}
+
+/**
+ * Format a string from tag data.
+ * Supported format fields:
+ * %s title (song)
+ * %l album
+ * %a artist
+ * %c comment
+ * %y year
+ * %t track, two digits, i.e. leading zero if < 10
+ * %T track, without leading zeroes
+ * %g genre
+ *
+ * @param format format specification
+ *
+ * @return formatted string.
+ */
+QString StandardTags::formatString(const QString& format) const
+{
+	if (!format.isEmpty()) {
+		const int numTagCodes = 8;
+		const QChar tagCode[numTagCodes] = {
+	    's', 'l', 'a', 'c', 'y', 't', 'T', 'g'
+		};
+		QString tagStr[numTagCodes];
+
+		QString yearStr, trackStr;
+		yearStr.sprintf("%d", year);
+		trackStr.sprintf("%02d", track);
+		tagStr[0] = title;
+		tagStr[1] = album;
+		tagStr[2] = artist;
+		tagStr[3] = comment;
+		tagStr[4] = yearStr;
+		tagStr[5] = trackStr;
+		tagStr[6] = QString::number(track);
+		tagStr[7] = Genres::getName(genre);
+		if (tagStr[7].isEmpty()) {
+			tagStr[7] = genreStr;
+		}
+
+		return replacePercentCodes(format, tagCode, tagStr, numTagCodes);
+	}
+	return format;
+}
+
 
 /**
  * If all fields are false set all true.
--- kid3/kid3/standardtags.h
+++ kid3/kid3/standardtags.h
@@ -44,6 +44,57 @@ class StandardTags {
 	 */
 	void setEmpty();
 
+	/**
+	 * Copy tags which are empty or inactive from other tags.
+	 * This can be used to merge two tags.
+	 *
+	 * @param st other tags
+	 */
+	void merge(const StandardTags& st);
+
+	/**
+	 * Format a string from tag data.
+	 * Supported format fields:
+	 * %s title (song)
+	 * %l album
+	 * %a artist
+	 * %c comment
+	 * %y year
+	 * %t track, two digits, i.e. leading zero if < 10
+	 * %T track, without leading zeroes
+	 * %g genre
+	 *
+	 * @param format format specification
+	 *
+	 * @return formatted string.
+	 */
+	QString formatString(const QString& format) const;
+
+	/**
+	 * Replace escaped characters in a string.
+	 *
+	 * @param format string with escaped two-character-sequences
+	 *               ("\n", "\t", "\r", "\\", "\a", "\b", "\f", "\v")
+	 *
+	 * @return string with escaped sequences replaced by characters.
+	 */
+	static QString replaceEscapedChars(const QString& format);
+
+	/**
+	 * Replace percent codes in a string.
+	 *
+	 * @param format string with percent codes
+	 *               (% followed by a single character)
+	 * @param codes        characters following percent
+	 * @param replacements strings with replacements for codes
+	 * @param numCodes     number of elements in codes and replacements
+	 *
+	 * @return string with percent codes replaced
+	 */
+	static QString replacePercentCodes(
+		const QString& format, const QChar* codes,
+		const QString* replacements, int numCodes);
+
 	/** Title, empty if "", inactive if QString::null */
 	QString title;
 	/** Artist, empty if "", inactive if QString::null */
