2e23a2b258c215382fabb169c98e8d3a162c6cbd
Urs Fleisch <ufleisch@users.sourceforge.net>
2009-02-24 21:03:15 +0000

    prepared some build files for version 1.2

--- kid3/build-deb.sh
+++ kid3/build-deb.sh
@@ -3,13 +3,17 @@
 mkdir debian
 cp deb/* debian/
 if test $(lsb_release -is) = "Ubuntu"; then
-	if test $(lsb_release -cs) != "hardy"; then
+  if test $(lsb_release -cs) != "hardy"; then
     sedcmds="s/^# Ubuntu://; s/^# KDE4://"
   else
     sedcmds="s/^# Ubuntu://; s/^# KDE3://"
   fi
 else
-  sedcmds="s/^# KDE3://"
+  if test $(lsb_release -cs) != "lenny"; then
+    sedcmds="s/^# KDE4://"
+  else
+    sedcmds="s/^# KDE3://"
+  fi
 fi
 sed -i "$sedcmds" debian/control
 dpkg-buildpackage -rfakeroot
