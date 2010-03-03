6d4c6f692f2f8afd8d81d1063136a1e61392ce7a
Urs Fleisch <ufleisch@users.sourceforge.net>
2010-03-03 16:46:36 +0000

    copy a DLL needed for GCC 4.4.0 on Windows/mingw

--- kid3/win32/createpkg.bat
+++ kid3/win32/createpkg.bat
@@ -2,5 +2,6 @@ mkdir kid3win\imageformats
 copy %QTDIR%\plugins\imageformats\qjpeg4.dll kid3win\imageformats
 for %%L in (Core Gui Network Xml) do copy %QTDIR%\bin\Qt%%L4.dll kid3win
 copy %QTDIR%\bin\mingwm10.dll kid3win
+copy %QTDIR%\bin\libgcc_s_dw2-1.dll kid3win
 for /f %%N in ('%MSYSDIR%\bin\perl -ne "print \"kid3-winbin-$1.zip\" if /Version:\s+([\d\.]+)/" ../kid3.lsm') do set ZIPNAME=%%N
 %ProgramFiles%\7-Zip\7z.exe a %ZIPNAME% kid3win
