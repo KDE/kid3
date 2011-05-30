a7804eb3edbb26aa5497fa2eee916673f51ba805
Urs Fleisch <ufleisch@users.sourceforge.net>
2011-05-30 19:15:26 +0200

    StartupWMClass of kid3-qt.desktop should be kid3-qt instead of kid3, fix by Ville Skyttä.

--- kid3/fixdesktop.pl
+++ kid3/fixdesktop.pl
@@ -3,4 +3,5 @@ s/^Name=Kid3$/Name=Kid3-qt/;
 s/^Exec=kid3/Exec=kid3-qt/;
 s/^Icon=kid3$/Icon=kid3-qt/;
 s/^Categories=Qt;KDE/Categories=Qt/;
+s/^StartupWMClass=kid3$/StartupWMClass=kid3-qt/;
 print unless /^X-DocPath/;
