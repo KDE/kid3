ce307bbe0a415afc587d484de6e0b3dc64f78a63
Urs Fleisch <ufleisch@users.sourceforge.net>
2011-06-11 16:53:52 +0200

    Disable mp4v2 by default, but still use it for Windows and Mac.
    Reason: Official Ubuntu and Debian builds do not use mp4v2 and the mp4v2
    version found in the repositories is quite outdated.

--- macosx/buildkid3.sh
+++ macosx/buildkid3.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX= -D WITH_TUNEPIMP=OFF ../kid3
+cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX= -D WITH_TUNEPIMP=OFF -D WITH_MP4V2=ON ../kid3
 make
 cpack
--- win32/buildkid3.bat
+++ win32/buildkid3.bat
@@ -3,6 +3,6 @@
 set PATH=%QTDIR%\bin;%QTDIR%\..\bin;%QTDIR%\..\mingw\bin;%MSYSDIR%\mingw\bin;C:\WINNT\System32;C:\Windows\System32;%PERLDIR%\bin;%DUMPBINDIR%;%CMAKEDIR%\bin
 set INCLUDE=%MSYSDIR%\local\include
 set LIB=%MSYSDIR%\local\lib
-cmake -G "MinGW Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX= -D WITH_TUNEPIMP=OFF -D WITH_MP4V2=OFF ..\kid3
+cmake -G "MinGW Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX= -D WITH_TUNEPIMP=OFF -D WITH_MP4V2=ON ..\kid3
 mingw32-make
 cpack
