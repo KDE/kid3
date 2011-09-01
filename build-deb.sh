24b64b0056cd6f5bcfac45d0e258944257f136c8
Urs Fleisch <ufleisch@users.sourceforge.net>
2011-09-01 10:10:35 +0200

    Remove KDE 3/Qt 3 support from build-deb.sh.

--- build-deb.sh
+++ build-deb.sh
@@ -2,18 +2,4 @@
 # Build Debian package.
 mkdir debian
 cp deb/* debian/
-if test $(lsb_release -is) = "Ubuntu"; then
-  if test $(lsb_release -cs) != "hardy"; then
-    sedcmds="s/^# Ubuntu://; s/^# KDE4://"
-  else
-    sedcmds="s/^# Ubuntu://; s/^# KDE3://"
-  fi
-else
-  if test $(lsb_release -cs) != "lenny"; then
-    sedcmds="s/^# KDE4://"
-  else
-    sedcmds="s/^# KDE3://"
-  fi
-fi
-sed -i "$sedcmds" debian/control
 dpkg-buildpackage -rfakeroot
