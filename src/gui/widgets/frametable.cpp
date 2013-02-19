7f7cc7ab502684d2c80fe8c348d99a113de604c0
Urs Fleisch <ufleisch@users.sourceforge.net>
2013-02-19 08:03:11 +0100

    Hide text fragments visible in header of frame table columns.

--- src/gui/widgets/frametable.cpp
+++ src/gui/widgets/frametable.cpp
@@ -53,6 +53,7 @@ FrameTable::FrameTable(FrameTableModel* model, QWidget* parent) :
   // Set a small height instead of hiding the header, so that the column
   // widths can still be resized by the user.
   horizontalHeader()->setFixedHeight(2);
+  horizontalHeader()->setStyleSheet(QLatin1String("color: rgba(0, 0, 0, 0);"));
   verticalHeader()->hide();
   if (model->isId3v1()) {
     bool insertTemporaryRow = model->rowCount() < 1;
