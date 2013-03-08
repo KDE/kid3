491367e473734a4cb65074d0f55e80fedae2ca07
Urs Fleisch <ufleisch@users.sourceforge.net>
2013-03-08 18:42:40 +0100

    Fixed build-deb.sh for Debian Squeeze.

--- build-deb.sh
+++ build-deb.sh
@@ -2,4 +2,8 @@
 # Build Debian package.
 cp -R deb debian
 rm -rf debian/source debian/watch
+if test "$(lsb_release -sc)" = "squeeze"; then
+  sed -i '/lib\(av\|chromaprint\)/ d' debian/control
+  sed -i 's/-DWITH_MP4V2=/-DWITH_CHROMAPRINT=OFF -DWITH_MP4V2=/; s/dh_builddeb -- -Zxz/dh_builddeb/' debian/rules
+fi
 dpkg-buildpackage -rfakeroot
