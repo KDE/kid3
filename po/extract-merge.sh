289242e3d566a3a8ba86a7bfafd8252f2c592181
Urs Fleisch <ufleisch@users.sourceforge.net>
2013-10-14 21:59:54 +0200

    Version 3.0.

--- po/extract-merge.sh
+++ po/extract-merge.sh
@@ -2,7 +2,7 @@
 
 BASEDIR="../src" # root of translatable sources
 PROJECT="kid3" # project name
-PROJECTVERSION="2.4" # project version
+PROJECTVERSION="3.0" # project version
 BUGADDR="http://sourceforge.net/p/kid3/bugs/" # MSGID-Bugs
 WDIR=`pwd` # working dir
 
