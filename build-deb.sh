c8a2e65715b540c1e12bb768bd33d4589c41d414
Urs Fleisch <ufleisch@users.sourceforge.net>
2015-04-19 14:28:20 +0200

    Adapt debian directory generated by deb-build.sh to Qt 5/KDE 5.

--- build-deb.sh
+++ build-deb.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Build Debian package.
 test -d debian && rm -rf debian
 cp -R deb debian
@@ -8,6 +8,43 @@ cp -R deb debian
 ppaversion=$1
 if test -n "$ppaversion"; then
   distribution=${ppaversion%%[0-9]*}
+  if which ubuntu-distro-info >/dev/null; then
+    if distrib_nr=$(ubuntu-distro-info --series=$distribution -r 2>/dev/null); then
+      distrib_nr=${distrib_nr/./}
+      distrib_id=Ubuntu
+    elif distrib_nr=$(debian-distro-info --series=$distribution -r 2>/dev/null); then
+      distrib_nr=${distrib_nr%.*}
+      distrib_id=Debian
+    else
+      echo "Could not find release number for $distribution"
+    fi
+  else
+    echo "distro-info is not installed"
+  fi
+else
+  distribution=$(lsb_release -sc)
+  distrib_nr=$(lsb_release -sr)
+  distrib_nr=${distrib_nr/./}
+  distrib_id=$(lsb_release -si)
+fi
+
+if test $distrib_id = "Ubuntu" -a $distrib_nr -ge 1504 ||
+   test $distrib_id = "Debian" -a $distrib_nr -ge 9; then
+  qtversion=5
+else
+  qtversion=4
+fi
+
+if test "$qtversion" = "5"; then
+  sed -i -e '/# KDE 4 BEGIN/,/# KDE 4 END/{/# KDE/b;s/^/#/};/# KDE 5 BEGIN/,/# KDE 5 END/{/# KDE/b;s/^#//}' \
+      debian/control debian/rules debian/kid3.install
+fi
+if test "$distribution" = "squeeze"; then
+  sed -i '/lib\(av\|chromaprint\)/ d' debian/control
+  sed -i 's/-DWITH_MP4V2=/-DWITH_CHROMAPRINT=OFF -DWITH_MP4V2=/; s/dh_builddeb -- -Zxz/dh_builddeb/' debian/rules
+fi
+
+if test -n "$ppaversion"; then
   version=$(sed -e 's/^kid3 (\([0-9\.-]\+\).*$/\1/;q' debian/changelog)
   DEBEMAIL="Urs Fleisch <ufleisch@users.sourceforge.net>" \
   dch --newversion=${version}${ppaversion} --distribution=$distribution --urgency=low \
@@ -18,9 +55,5 @@ if test -n "$ppaversion"; then
   echo "cd ..; dput ppa:ufleisch/kid3 kid3_${version}${ppaversion}_source.changes"
 else
   rm -rf debian/source debian/watch
-  if test "$(lsb_release -sc)" = "squeeze"; then
-    sed -i '/lib\(av\|chromaprint\)/ d' debian/control
-    sed -i 's/-DWITH_MP4V2=/-DWITH_CHROMAPRINT=OFF -DWITH_MP4V2=/; s/dh_builddeb -- -Zxz/dh_builddeb/' debian/rules
-  fi
-  dpkg-buildpackage -rfakeroot
+  debuild
 fi
