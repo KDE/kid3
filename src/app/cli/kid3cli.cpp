daea79a19e96ce7c09064248b0f51c2304709c75
Urs Fleisch <ufleisch@users.sourceforge.net>
2015-11-01 18:54:10 +0100

    List picture in get command of kid3-cli also if its description is empty.
    http://sourceforge.net/p/kid3/discussion/241228/thread/18f82cf6/

--- src/app/cli/kid3cli.cpp
+++ src/app/cli/kid3cli.cpp
@@ -536,7 +536,10 @@ void Kid3Cli::writeFileInformation(int tagMask)
               ft->index(row, FrameTableModel::CI_Enable).data().toString();
           QString value =
               ft->index(row, FrameTableModel::CI_Value).data().toString();
-          if (!value.isEmpty()) {
+          if (!value.isEmpty() ||
+              ft->index(row, FrameTableModel::CI_Enable).
+              data(FrameTableModel::FrameTypeRole).toInt() == Frame::FT_Picture)
+          {
             bool changed = ft->index(row, FrameTableModel::CI_Enable).
                 data(Qt::BackgroundColorRole).value<QBrush>() != Qt::NoBrush;
             QString line = changed ? QLatin1String("*") : QLatin1String(" ");
