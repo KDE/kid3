cfb9d1520c930f0f162a4d541d477c277bb71120
Urs Fleisch <ufleisch@users.sourceforge.net>
2016-08-19 14:19:53 +0200

    Disable the global app menu on Ubuntu.
    Keyboard shortcuts are not displayed, and they do not work unless
    their action is visible in a toolbar. Until these bugs are fixed
    it is better not to use the global menu.

--- src/app/qt/fixdesktop.py
+++ src/app/qt/fixdesktop.py
@@ -2,13 +2,25 @@
 
 import fileinput
 import sys
+import platform
 
 
 def fix_desktop():
+    # Disable the global app menu on Ubuntu. Keyboard shortcuts are not
+    # displayed, and they do not work unless their action is visible
+    # in a toolbar.
+    try:
+        is_ubuntu = platform.linux_distribution()[0].lower() == 'ubuntu'
+    except:
+        is_ubuntu = False
+    if is_ubuntu:
+        exec_start = 'Exec=env UBUNTU_MENUPROXY=0 kid3-qt'
+    else:
+        exec_start = 'Exec=kid3-qt'
     for line in fileinput.input():
         line = line\
           .replace('Name=Kid3', 'Name=Kid3-qt')\
-          .replace('Exec=kid3', 'Exec=kid3-qt')\
+          .replace('Exec=kid3', exec_start)\
           .replace('Icon=kid3', 'Icon=kid3-qt')\
           .replace('Categories=Qt;KDE', 'Categories=Qt')\
           .replace('StartupWMClass=kid3', 'StartupWMClass=kid3-qt')
