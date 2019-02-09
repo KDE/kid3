b3bd70ed02490fb7ccc75c6eacf5030705f4ed49
Urs Fleisch <ufleisch@users.sourceforge.net>
2019-02-09 15:52:40 +0100

    Fix discogs import, has now '| Discogs' in title, [bugs:#191].

--- src/plugins/discogsimport/discogsimporter.cpp
+++ src/plugins/discogsimport/discogsimporter.cpp
@@ -265,7 +265,7 @@ void DiscogsImporter::parseFindResults(const QByteArray& searchStr)
 void DiscogsImporter::parseAlbumResults(const QByteArray& albumStr)
 {
   QRegExp nlSpaceRe(QLatin1String("[\r\n]+\\s*"));
-  QRegExp atDiscogsRe(QLatin1String("\\s*\\([^)]+\\) at Discogs\n?$"));
+  QRegExp atDiscogsRe(QLatin1String("\\s*\\([^)]+\\) (?:at|\\|) Discogs\n?$"));
   QString str = QString::fromUtf8(albumStr);
 
   FrameCollection framesHdr;
