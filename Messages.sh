26e637931114fb6d0674af3ace2ddf1fd38d0d72
Albert Astals Cid <aacid@kde.org>
2020-01-10 23:48:27 +0100

    Rename the output pot to make it clear it's a Qt only .pot

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-$EXTRACT_TR_STRINGS `find . -name \*.qml -o -name \*.cpp -o -name \*.h` -o $podir/kid3.pot
+$EXTRACT_TR_STRINGS `find . -name \*.qml -o -name \*.cpp -o -name \*.h` -o $podir/kid3_qt.pot
