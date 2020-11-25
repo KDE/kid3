1a088fe411c84ae086c06e740a8920d239c5310d
Urs Fleisch <ufleisch@users.sourceforge.net>
2020-11-25 21:40:17 +0100

    Simplify downloading translations via CMake

--- doc/download-pos.cmake
+++ doc/download-pos.cmake
@@ -3,18 +3,31 @@
 # CMAKE_ARGV0, CMAKE_ARGV1, CMAKE_ARGV2 are
 # /path/to/cmake, -P, /path/to/download-pos.cmake
 set(_languages ${CMAKE_ARGV3})
-if(NOT CMAKE_VERSION VERSION_LESS "3.11")
-  include(FetchContent)
-  FetchContent_Populate(l10nkf5_docmessages_pot
-    SVN_REPOSITORY svn://anonsvn.kde.org/home/kde/trunk/l10n-kf5/templates/docmessages/kid3
-    SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/po")
-  foreach(_lang ${_languages})
-    string(REPLACE "@" "" _lang_simplified ${_lang})
-    FetchContent_Populate(l10nkf5_docmessages_${_lang_simplified}
-      SVN_REPOSITORY svn://anonsvn.kde.org/home/kde/trunk/l10n-kf5/${_lang}/docmessages/kid3
-      SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/po/${_lang}")
-    FetchContent_Populate(l10nkf5_docs_${_lang}
-      SVN_REPOSITORY svn://anonsvn.kde.org/home/kde/trunk/l10n-kf5/${_lang}/docs/kid3/kid3
-      SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/docs/${_lang}")
-  endforeach()
+set(_urlprefix "https://websvn.kde.org/*checkout*/trunk/l10n-kf5")
+set(_podir "${CMAKE_CURRENT_LIST_DIR}/po")
+set(_docsdir "${CMAKE_CURRENT_LIST_DIR}/docs")
+set(_hdr "User-Agent: Mozilla/5.0")
+set(_url "${_urlprefix}/templates/docmessages/kid3/kid3.pot")
+message(STATUS "Downloading po, docs")
+file(DOWNLOAD ${_url} "${_podir}/kid3.pot"
+  HTTPHEADER "${_hdr}"
+  STATUS _status)
+if(NOT _status MATCHES "^0;")
+  message(FATAL_ERROR "${_url}: ${_status}")
 endif()
+foreach(_lang ${_languages})
+  set(_url "${_urlprefix}/${_lang}/docmessages/kid3/kid3.po")
+  file(DOWNLOAD ${_url} "${_podir}/${_lang}/kid3.po"
+    HTTPHEADER "${_hdr}"
+    STATUS _status)
+  if(NOT _status MATCHES "^0;")
+    message(FATAL_ERROR "${_url}: ${_status}")
+  endif()
+  set(_url "${_urlprefix}/${_lang}/docs/kid3/kid3/index.docbook")
+  file(DOWNLOAD ${_url} "${_docsdir}/${_lang}/index.docbook"
+    HTTPHEADER "${_hdr}"
+    STATUS _status)
+  if(NOT _status MATCHES "^0;")
+    message(FATAL_ERROR "${_url}: ${_status}")
+  endif()
+endforeach()
--- translations/download-pos.cmake
+++ translations/download-pos.cmake
@@ -3,15 +3,23 @@
 # CMAKE_ARGV0, CMAKE_ARGV1, CMAKE_ARGV2 are
 # /path/to/cmake, -P, /path/to/download-pos.cmake
 set(_languages ${CMAKE_ARGV3})
-if(NOT CMAKE_VERSION VERSION_LESS "3.11")
-  include(FetchContent)
-  FetchContent_Populate(l10nkf5_messages_pot
-    SVN_REPOSITORY svn://anonsvn.kde.org/home/kde/trunk/l10n-kf5/templates/messages/kid3
-    SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/po")
-  foreach(_lang ${_languages})
-    string(REPLACE "@" "" _lang_simplified ${_lang})
-    FetchContent_Populate(l10nkf5_messages_${_lang_simplified}
-      SVN_REPOSITORY svn://anonsvn.kde.org/home/kde/trunk/l10n-kf5/${_lang}/messages/kid3
-      SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/po/${_lang}")
-  endforeach()
+set(_urlprefix "https://websvn.kde.org/*checkout*/trunk/l10n-kf5")
+set(_podir "${CMAKE_CURRENT_LIST_DIR}/po")
+set(_hdr "User-Agent: Mozilla/5.0")
+set(_url "${_urlprefix}/templates/messages/kid3/kid3_qt.pot")
+message(STATUS "Downloading po")
+file(DOWNLOAD ${_url} "${_podir}/kid3_qt.pot"
+  HTTPHEADER "${_hdr}"
+  STATUS _status)
+if(NOT _status MATCHES "^0;")
+  message(FATAL_ERROR "${_url}: ${_status}")
 endif()
+foreach(_lang ${_languages})
+  set(_url "${_urlprefix}/${_lang}/messages/kid3/kid3_qt.po")
+  file(DOWNLOAD ${_url} "${_podir}/${_lang}/kid3_qt.po"
+    HTTPHEADER "${_hdr}"
+    STATUS _status)
+  if(NOT _status MATCHES "^0;")
+    message(FATAL_ERROR "${_url}: ${_status}")
+  endif()
+endforeach()
