9488baeac5175a063c44fe4a84618c1b49302187
Urs Fleisch <ufleisch@users.sourceforge.net>
2022-09-22 07:07:12 +0200

    Fix Discogs import

--- src/plugins/discogsimport/discogsimporter.cpp
+++ src/plugins/discogsimport/discogsimporter.cpp
@@ -740,7 +740,7 @@ void DiscogsImporter::HtmlImpl::parseFindResults(const QByteArray& searchStr)
   // <a class="search_result_title " href="/Amon-Amarth-The-Avenger/release/761529-Amon-Amarth-The-Avenger" data-followable="true">The Avenger</a>
   QString str = QString::fromUtf8(searchStr);
   QRegularExpression idTitleRe(QLatin1String(
-      "<a href=\"/artist/[^>]+?>([^<]+?)</a>[^-]*?-"
+      "href=\"/artist/[^>]+?>([^<]+?)</a>[^-]*?-"
       "\\s*?<a class=\"search_result_title[ \"]+?href=\"/([^/]*?/?release)/"
       "([0-9]+-[^\"]+?)\"[^>]*?>([^<]+?)</a>(.*?card_actions)"),
        QRegularExpression::DotMatchesEverythingOption);
