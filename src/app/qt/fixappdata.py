7e50fc54ed70839035cde5556ede18f45d6ac954
Urs Fleisch <ufleisch@users.sourceforge.net>
2023-11-26 12:07:29 +0100

    Fix Freedesktop AppStream appdata
    The build job on KDE GitLab failed because 'appstreamcli validate'
    issued a warning for org.kde.kid3-qt.appdata.xml:
    cid-rdns-contains-hyphen org.kde.kid3-qt.desktop
    Now the hyphen in the id is replaced by an underscore.
    Another information 'desktop-app-launchable-omitted' is also fixed
    by adding a 'launchable' tag. The 'developer-name-tag-deprecated'
    info is not fixed by now because it would include a lot of
    translations.

--- src/app/qt/fixappdata.py
+++ src/app/qt/fixappdata.py
@@ -6,7 +6,8 @@ import sys
 
 def fix_appdata():
     for line in fileinput.input(mode='rb'):
-        line = line.replace(b'kid3.desktop', b'kid3-qt.desktop')
+        line = line.replace(b'kid3.desktop</id>', b'kid3_qt.desktop</id>') \
+                   .replace(b'kid3.desktop', b'kid3-qt.desktop')
         sys.stdout.buffer.write(line)
 
 if __name__ == '__main__':
