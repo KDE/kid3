28f4dcef2f03dc4ee4dc5c85d7f2e08be3998047
Urs Fleisch <ufleisch@users.sourceforge.net>
2024-08-04 17:01:04 +0200

    Add '-qt' to the name in org.kde.kid3-qt.appdata.xml

--- src/app/qt/fixappdata.py
+++ src/app/qt/fixappdata.py
@@ -7,7 +7,8 @@ import sys
 def fix_appdata():
     for line in fileinput.input(mode='rb'):
         line = line.replace(b'kid3.desktop</id>', b'kid3_qt.desktop</id>') \
-                   .replace(b'kid3.desktop', b'kid3-qt.desktop')
+                   .replace(b'kid3.desktop', b'kid3-qt.desktop') \
+                   .replace(b'<name>Kid3</name>', b'<name>Kid3-qt</name>')
         sys.stdout.buffer.write(line)
 
 
