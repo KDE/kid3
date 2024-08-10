01a54cdae02dd6b83132b59e0e84e1a008e2fc63
Urs Fleisch <ufleisch@users.sourceforge.net>
2024-08-10 16:07:46 +0200

    mac-codesign.cmake: Try to sign with KDE CI tools

--- macosx/mac-codesign.cmake
+++ macosx/mac-codesign.cmake
@@ -1,9 +1,3 @@
-if(NOT DEFINED ENV{SIGNING_IDENTITY})
-  message(WARNING "Environment variable SIGNING_IDENTITY not set, not signing")
-  return()
-endif()
-set(SIGNING_IDENTITY $ENV{SIGNING_IDENTITY})
-set(CODESIGN codesign --force --sign "${SIGNING_IDENTITY}" --options=runtime)
 if(CMAKE_SCRIPT_MODE_FILE AND NOT CMAKE_PARENT_LIST_FILE)
   set(_pathToParent "${CMAKE_CURRENT_SOURCE_DIR}/./")
 else()
@@ -11,6 +5,31 @@ else()
 endif()
 set(_bundleName "kid3.app")
 set(_pathToBundle "${_pathToParent}/${_bundleName}")
+
+if(NOT DEFINED ENV{SIGNING_IDENTITY})
+  message(WARNING "Environment variable SIGNING_IDENTITY not set, not signing")
+
+  # Try to sign using the KDE CI tools
+  if(EXISTS $ENV{CI_PROJECT_DIR}/ci-notary-service/signmacapp.py AND
+     EXISTS $ENV{CI_PROJECT_DIR}/ci-utilities/signing/signmacapp.ini)
+    find_package(Python3 COMPONENTS Interpreter REQUIRED)
+    set(PYTHON_EXECUTABLE "${Python3_EXECUTABLE}")
+    execute_process(
+      COMMAND ${PYTHON_EXECUTABLE} ci-notary-service/signmacapp.py -v
+        --config ci-utilities/signing/signmacapp.ini ${_pathToBundle}
+      WORKING_DIRECTORY "$ENV{CI_PROJECT_DIR}"
+      RESULT_VARIABLE _result
+    )
+    if(NOT (${_result} EQUAL 0))
+      message(WARNING "signmacapp.py ${_pathToBundle} failed with ${_result}")
+    endif()
+  endif()
+
+  return()
+endif()
+
+set(SIGNING_IDENTITY $ENV{SIGNING_IDENTITY})
+set(CODESIGN codesign --force --sign "${SIGNING_IDENTITY}" --options=runtime)
 
 execute_process(
   COMMAND xcrun -f codesign_allocate
