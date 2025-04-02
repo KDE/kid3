KDE/kid3

commit 3becddda57e9cb5c6f01853d199a4ef10da2eb43

QML: Fix isStandalone() when running non-interactively with kid3-cli
In QML scripts, isStandalone() is used to check if the script is not
returning to the Kid3 UI and thus it has to save modified files itself
as there is no user which can do that interactively afterwards.
Then a QML script is started from the UI, isStandalone() returns false,
but when it is started using qml or qmlscene, it returns true.
As QML scripts can now also be started with kid3-cli, they should get a
false result, when the "execute @qml script" command is called
interactively from a kid3-cli session, but a true result, if kid3-cli is
only invoking the script without an interactive session, e.g. using
kid3-cli -c "execute @qml script". The last case was not handled
correctly and is now fixed.

src/plugins/qmlcommand/qmlcommandplugin.cpp
@@ -96,11 +96,13 @@ void QmlCommandPlugin::cleanup()
  * @param key user command name, "qml" or "qmlview"
  * @param arguments arguments to pass to script
  * @param showOutput true to enable output in output viewer, using signal
- *                   commandOutput().
+ *                   commandOutput()
+ * @param vars variables to make available in command
  * @return true if command is started.
  */
 bool QmlCommandPlugin::startUserCommand(
-    const QString& key, const QStringList& arguments, bool showOutput)
+    const QString& key, const QStringList& arguments, bool showOutput,
+    const QVariantMap& vars)
 {
   if (!arguments.isEmpty()) {
     if (key == QLatin1String("qmlview")) {
@@ -118,6 +120,8 @@ bool QmlCommandPlugin::startUserCommand(
       }
       m_qmlView->engine()->rootContext()->setContextProperty(
             QLatin1String("args"), arguments);
+      m_qmlEngine->rootContext()->setContextProperty(
+            QLatin1String("vars"), vars);
       onEngineReady();
       m_qmlView->setSource(QUrl::fromLocalFile(arguments.first()));
       if (m_qmlView->status() == QQuickView::Ready) {
@@ -144,6 +148,8 @@ bool QmlCommandPlugin::startUserCommand(
       }
       m_qmlEngine->rootContext()->setContextProperty(QLatin1String("args"),
                                                      arguments);
+      m_qmlEngine->rootContext()->setContextProperty(QLatin1String("vars"),
+                                                     vars);
       if (QQmlComponent component(m_qmlEngine, arguments.first());
         component.status() == QQmlComponent::Ready) {
         onEngineReady();

src/app/cli/clicommand.cpp
@@ -1437,7 +1437,8 @@ void ExecuteCommand::startCommand()
       connectResultSignal();
     }
     m_process->setOutputViewer(this);
-    if (!m_process->launchCommand(command, args().mid(1), true)) {
+    QVariantMap vars{{QLatin1String("standalone"), !cli()->isInteractive()}};
+    if (!m_process->launchCommand(command, args().mid(1), true, vars)) {
       setError(tr("Could not execute ") + args().mid(1).join(QLatin1String(" ")));
       terminate();
     }

src/app/cli/kid3cli.cpp
@@ -177,7 +177,8 @@ Kid3Cli::Kid3Cli(Kid3Application* app,
                  AbstractCliIO* io, const QStringList& args, QObject* parent) :
   AbstractCli(io, parent),
   m_app(app), m_args(args),
-  m_tagMask(Frame::TagV2V1), m_timeoutMs(0), m_fileNameChanged(false)
+  m_tagMask(Frame::TagV2V1), m_timeoutMs(0), m_fileNameChanged(false),
+  m_isInteractive(false)
 {
   m_formatters << new JsonCliFormatter(io)
                << new TextCliFormatter(io);
@@ -346,7 +347,8 @@ void Kid3Cli::writeHelp(const QString& cmdName, bool usageMessage)
  */
 void Kid3Cli::execute()
 {
-  if (!parseOptions()) {
+  m_isInteractive = !parseOptions();
+  if (m_isInteractive) {
     // Interactive mode
     AbstractCli::execute();
   }

src/app/cli/kid3cli.h
@@ -189,6 +189,12 @@ public:
   void setTimeout(int timeout) { m_timeoutMs = timeout; }
 
   /**
+   * Check if command line is interactive.
+   * @return true if CLI is running in interactive mode.
+   */
+  bool isInteractive() const { return m_isInteractive; }
+
+  /**
    * Expand wildcards in path list.
    * @param paths paths to expand
    * @return expanded paths.
@@ -260,4 +266,5 @@ private:
   /** Overwrites command timeout, -1 to switch off, 0 for defaults, else ms. */
   int m_timeoutMs;
   bool m_fileNameChanged;
+  bool m_isInteractive;
 };

src/core/model/externalprocess.cpp
@@ -76,10 +76,11 @@ ExternalProcess::~ExternalProcess()
  * @param name       display name
  * @param args       command and arguments
  * @param showOutput true to show output of process
+ * @param vars       variables to make available in command
  * @return false if process could not be executed.
  */
 bool ExternalProcess::launchCommand(const QString& name, const QStringList& args,
-                                    bool showOutput)
+                                    bool showOutput, const QVariantMap& vars)
 {
   if (args.isEmpty())
     return true;
@@ -115,7 +116,7 @@ bool ExternalProcess::launchCommand(const QString& name, const QStringList& args
         connect(userCommandProcessor->qobject(), SIGNAL(finished(int)),
                 this, SIGNAL(finished(int)), Qt::UniqueConnection);
         if (userCommandProcessor->startUserCommand(program, arguments,
-                                                   showOutput)) {
+                                                   showOutput, vars)) {
           return true;
         }
       }

src/core/model/externalprocess.h
@@ -28,6 +28,7 @@
 
 #include <QObject>
 #include <QStringList>
+#include <QVariantMap>
 #include "kid3api.h"
 
 class QProcess;
@@ -98,10 +99,12 @@ public:
    * @param name       display name
    * @param args       command and arguments
    * @param showOutput true to show output of process
+   * @param vars       variables to make available in command
    * @return false if process could not be executed.
    */
   bool launchCommand(const QString& name, const QStringList& args,
-                     bool showOutput = false);
+                     bool showOutput = false,
+                     const QVariantMap& vars = QVariantMap());
 
 signals:
   /**

src/core/model/iusercommandprocessor.h
@@ -28,6 +28,7 @@
 
 #include <QtPlugin>
 #include <QStringList>
+#include <QVariantMap>
 #include "kid3api.h"
 
 class Kid3Application;
@@ -67,6 +68,7 @@ public:
    * @param key user command name
    * @param arguments arguments to pass to command
    * @param showOutput true to enable output in output viewer
+   * @param vars variables to make available in command
    * @return true if command is started.
    *
    * @remarks If @a showOutput is true, command output is emitted using a signal
@@ -75,7 +77,8 @@ public:
    * @see qobject()
    */
   virtual bool startUserCommand(
-      const QString& key, const QStringList& arguments, bool showOutput) = 0;
+      const QString& key, const QStringList& arguments, bool showOutput,
+      const QVariantMap& vars = QVariantMap()) = 0;
 
   /**
    * Return object which emits commandOutput() and finished() signals.

src/plugins/kid3qml/Kid3Script.qml
@@ -179,7 +179,8 @@ Timer {
    * Check if script is running stand-alone, i.e. not in the Kid3 application.
    */
   function isStandalone() {
-    return typeof args === "undefined"
+    return typeof vars === "object"
+        ? !!vars.standalone : typeof vars === "undefined"
   }
 
   /**

commit e6a7717b9a2b05a4373c55b138974fc80d8f72d1

Fix crash with QML view (e.g. QML console)
This bug was introduced in 3becddda
QML: Fix isStandalone() when running non-interactively with kid3-cli

src/plugins/qmlcommand/qmlcommandplugin.cpp
@@ -120,7 +120,7 @@ bool QmlCommandPlugin::startUserCommand(
       }
       m_qmlView->engine()->rootContext()->setContextProperty(
             QLatin1String("args"), arguments);
-      m_qmlEngine->rootContext()->setContextProperty(
+      m_qmlView->engine()->rootContext()->setContextProperty(
             QLatin1String("vars"), vars);
       onEngineReady();
       m_qmlView->setSource(QUrl::fromLocalFile(arguments.first()));