73e6e8e2c71c142d867119e61154face3064bb3a
Urs Fleisch <ufleisch@users.sourceforge.net>
2025-06-22 13:33:12 +0200

    Improve AppStream metadata
    Fix component type, translation type.
    Add project_group, provides, categories.
    Remove replaces.

--- src/app/qt/fixappdata.py
+++ src/app/qt/fixappdata.py
@@ -6,8 +6,11 @@ import sys
 
 def fix_appdata():
     for line in fileinput.input(mode='rb'):
+        if b'<project_group>KDE</project_group>' in line:
+            continue
         line = line.replace(b'kid3</id>', b'kid3-qt</id>') \
-                   .replace(b'kid3.desktop</id>', b'kid3_qt.desktop</id>') \
+                   .replace(b'kid3</binary>', b'kid3-qt</binary>') \
+                   .replace(b'KDE</category>', b'Qt</category>') \
                    .replace(b'kid3.desktop', b'kid3-qt.desktop') \
                    .replace(b'>Kid3</name>', b'>Kid3-qt</name>')
         sys.stdout.buffer.write(line)
